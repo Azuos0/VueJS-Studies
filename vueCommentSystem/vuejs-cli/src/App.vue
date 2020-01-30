9c13f0b5705d2f8ae7b075c076d9bf97d4b408fb
Igor Vieira <igorvieiradsz@gmail.com>
2020-01-30 00:24:12 -0300

    Adding WeatherApp and CommentApp

---
+++ vueCommentSystem/vuejs-cli/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <comments/>
+</template>
+
+<script>
+import Comments from './components/Comments.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Comments
+  }
+}
+</script>
