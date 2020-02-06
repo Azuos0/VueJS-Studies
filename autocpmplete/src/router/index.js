3e4c604a0ed28686e92e2a82d97e0572bdab0bca
Igor Vieira <igorvieiradsz@gmail.com>
2020-02-06 18:00:00 -0300

    Auto format input fields

--- autocpmplete/src/router/index.js
+++ autocpmplete/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
 import Pricing from '../views/Pricing';
+import Forms from '../views/Forms';
 
 Vue.use(VueRouter);
 
@@ -15,6 +16,11 @@ const routes = [
     path: '/pricing',
     name: 'pricing',
     component: Pricing
+  },
+  {
+    path: '/forms',
+    name: 'form',
+    component: Forms
   },
   {
     path: '/about',
---
+++ autocpmplete/src/views/Forms.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <Telephone template="XXX.XXX.XXX-XX"></Telephone>
+    </div>
+</template>
+
+<script>
+import Telephone from '../components/Telephone'
+export default {
+    name: 'forms',
+    components: {
+        Telephone
+    },
+
+}
+</script>
